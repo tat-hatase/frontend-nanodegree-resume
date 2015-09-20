a908920fffb203e1e7d0b0231c9a8888921e3218
tat <hataset@onid,oregonstate.edu>
2015-09-20 14:02:52 -0700

    Add personal information to resumeBuilder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,78 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	"name" : "tat hatase",
+	"role" : "Math Instructor",
+	"contacts" : {
+      "mobile": "541-908-4483",
+      "email": "hataset@onid.orst.edu",
+      "github": "tat-hatase",
+      "twitter": "@TweetForTat",
+      "location": "Corvallis, OR"
+	},
+	"welcomeMessage": "Welcome to my resume page!",
+	"skills": ["strategic problem solving", "teaching", "some computer stuff"],
+	"biopic": "img/profile.jpg",
+	}
+}
+
+var education = {
+	"schools": [{
+      "name": "Oregon State University",
+      "location": "Corvallis, OR",
+      "degree": "BS",
+      "majors": "Mathematics",
+      "dates": 2004,
+      "url": "http://oregonstate.edu/"
+	  },
+	  {
+      "name": "Oregon State University",
+      "location": "Corvallis, OR",
+      "degree": "MS",
+      "majors": "Mathematics",
+      "dates": 2006,
+      "url": "http://oregonstate.edu/"
+	  },
+	  {
+      "name": "Oregon State University",
+      "location": "Corvallis, OR",
+      "degree": "PhD",
+      "majors": "Mathematics",
+      "dates": 2011,
+      "url": "http://oregonstate.edu/"
+	  }],
+	"onlineCourses": [{
+      "title": "Intro to Programming Nanodegree",
+      "school": "Udacity",
+      "date": 2015,
+      "url": "https://www.udacity.com"
+	  }],
+	}
+}
+
+var work = {
+	"jobs": [{
+      "employer": "Oregon State University Department of Mathematics",
+      "title": "Instructor",
+      "location": "Corvallis, OR",
+      "dates": "2011-Present",
+      "description": "Teaching lower to mid level math courses.",
+	  "url": "http://www.math.oregonstate.edu/"
+	  },
+	  {
+	  "employer": "Oregon State University Department of Mathematics",
+      "title": "Graduate Teaching Assistant",
+      "location": "Corvallis, OR",
+      "dates": "2004-2011",
+      "description": "Assisting in lower to mid level math courses by facilitating recitations, grading quizzes, homework, and worksheets, proctoring exams, and answering any questions students may have.",
+	  "url": "http://www.math.oregonstate.edu/"
+	  }],
+}
+
+var projects = {
+	"projects": [{
+      "title": "PhD Thesis",
+      "dates": "Completed Summer 2011",
+      "description": "Algebraic Pappus Curves",
+      "images": ["img/Excellent.jpg", "img/logo.jpg", "img/Photobomb.jpg"],
+	  "url": "https://ir.library.oregonstate.edu/xmlui/bitstream/handle/1957/23320/Tatsuhiko%20Hatase%20-%20Thesis.pdf?sequence=1"
+	  }],
+}
